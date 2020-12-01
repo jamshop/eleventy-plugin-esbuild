df4f0bd6bf3e9e107affd186881e917f4ef6707c
MadeByMike <mike@madebymike.com.au>
2020-12-02 02:58:26 +1100

    Version 1

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { buildSync } = require("esbuild");
-const  addCachedGlobalData = require("./addCachedGlobalData");
+const addCachedGlobalData = require("./addCachedGlobalData");
 const esBuildTransform = require("./transform");
 const esBuildShortcode = require("./shortcode");
 const path = require("path");
 
-const compileEsBuildTargets = (entryPoints, output, esbuild={}) => {
+const compileEsBuildTargets = (entryPoints, output, esbuild = {}) => {
   // entryPoints:
   // [{ main: "/path/to.js"}]
 
@@ -18,7 +18,7 @@ const compileEsBuildTargets = (entryPoints, output, esbuild={}) => {
     const file = files[index][1];
 
     let outfile = undefined;
-    if(output) {
+    if (output) {
       outfile = path.join(output, `${key}.js`);
     }
 
@@ -28,19 +28,22 @@ const compileEsBuildTargets = (entryPoints, output, esbuild={}) => {
       bundle: true,
       write: output ? true : false,
       outfile,
-      ...esbuild
+      ...esbuild,
     });
-    if(res.outputFiles) {
+    if (res.outputFiles) {
       data[key] = new TextDecoder("utf-8").decode(res.outputFiles[0].contents);
     }
   }
   return data;
 };
 
-const esBuildPlugin = (eleventyConfig, { entryPoints = {}, output, esbuild } = {}) => {
+const esBuildPlugin = (
+  eleventyConfig,
+  { entryPoints = {}, output, esbuild } = {}
+) => {
   if (Object.entries(entryPoints).length === 0) {
     // Support older versions of 11ty
-    if(eleventyConfig.addGlobalData) {
+    if (eleventyConfig.addGlobalData) {
       // return an empty object if no enteries
       eleventyConfig.addGlobalData("esbuild", {});
     }
@@ -50,23 +53,33 @@ const esBuildPlugin = (eleventyConfig, { entryPoints = {}, output, esbuild } = {
     );
     return;
   }
- 
- 
-  if(eleventyConfig.addGlobalData) {
-     addCachedGlobalData(
-      eleventyConfig,
-      () => compileEsBuildTargets(entryPoints, output, esbuild),
-      "esbuild"
-    );
-  } else {
-    compileEsBuildTargets(entryPoints, output, esbuild);
-  }
 
-  // Ideally we'd add a watch for each assets consumed by the entry but as yet I don't think that can be done with esbuild
+  const doBuild = () => {
+    if (eleventyConfig.addGlobalData) {
+      addCachedGlobalData(
+        eleventyConfig,
+        () => compileEsBuildTargets(entryPoints, output, esbuild),
+        "esbuild"
+      );
+    } else {
+      compileEsBuildTargets(entryPoints, output, esbuild);
+    }
+  }
+  doBuild();
+ 
+   // Ideally we'd add a watch for each assets consumed by the entry, but as yet I don't think that can be done with esbuild
+  let watchPaths = [];
   Object.entries(entryPoints).forEach(([_, watchPath]) => {
-    const relativePath = path.relative(process.cwd(), watchPath);
-    eleventyConfig.addWatchTarget(relativePath);
+    watchPaths.push(watchPath);
+    eleventyConfig.addWatchTarget(watchPath);
   });
+  eleventyConfig.on("beforeWatch", (changedFiles) => {
+    // Run me before --watch or --serve re-runs
+    if(watchPaths.some(watchPath => changedFiles.includes(watchPath))) {
+      doBuild();
+    }
+  });
+
 
 };
 
