16010484fe5cfa57417777d1d5d03965ffa84fb3
Peter Janes <peter.janes@autodata.net>
2021-05-18 11:33:18 -0400

    Update to use esbuild 0.9

--- tests/stubs/_out/main-react.js
+++ tests/stubs/_out/main-react.js
@@ -1,1 +1,1 @@
-(()=>{var t=(e,p)=>()=>(p||(p={exports:{}},e(p.exports,p)),p.exports),L=t((n,r)=>{r.exports=()=>React.createElement("p",null,"HELLO WORLD")});L();})();
+(()=>{var t=(e,p)=>()=>(p||(p={exports:{}},e(p.exports,p)),p.exports);var L=t((n,r)=>{r.exports=()=>React.createElement("p",null,"HELLO WORLD")});L();})();
